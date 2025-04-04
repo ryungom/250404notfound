0df502bbf9d85c1e75c8e733380b8b8b9d239d5b
류재준 <ryungom@gmail.com>
2025-04-05 02:49:04 +0900

    feat: ogtitle 및 메인페이지 링크 수정

--- src/app/(web)/LinkPage.tsx
+++ src/app/(web)/LinkPage.tsx
@@ -21,29 +21,30 @@ export default function LinkPage() {
   ];
   const isActive = (input: string) => {
     if (input === params.case || (input === "/" && params.case === undefined)) {
-      return "bg-[#555]";
+      return "bg-[#555] active";
     } else {
       return "bg-[#ddd]";
     }
   };
   const changeLinks = gnb.map((item) => {
-    return <Link key={`${item.route}`} href={`${item.route}`} className={`w-[10px] h-[10px] rounded-[50%] block ${isActive(item.route)}`}></Link>;
+    return <Link key={`${item.route}`} href={`${item.route}`} className={`w-[15px] h-[15px] rounded-[50%] block duration-100 ease-linear ${isActive(item.route)}`}></Link>;
   });
 
   return (
     <>
-      <aside className="absolute bottom-[50px] translate-x-[-50%] left-[50%] flex flex-col items-center">
+      <aside className="absolute bottom-[50px] translate-x-[-50%] left-[50%] w-full max-w-[150px] flex flex-col items-center">
         {/* <button className="border-1 px-2">
               대통령
               <br />
               새로고침
             </button> */}
-        <div className="flex justify-between mb-[30px] w-full max-w-[150px]">{changeLinks}</div>
-        <button className="underline w-full max-w-[200px]">
+        <div className="flex justify-between mb-[30px] w-full relative linkBtnWrap">{changeLinks}</div>
+        <span className="text-[#999]">잘가시게</span>
+        {/* <button className="underline w-full max-w-[200px]">
           <Link href="https://www.criminalyoon.com/" className="w-full h-full block px-2" target="_blank">
             내란범죄 동조명단
           </Link>
-        </button>
+        </button> */}
       </aside>
     </>
   );
--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -9,15 +9,19 @@ export default function Home() {
         <figure className="relative aspect-square w-full max-w-[150px] bgMotion mb-2">
           <Image src="./image/oops.svg" fill sizes="100%" alt=""></Image>
         </figure>
-        <h2 className="text-[1.8rem] text-red-500">대통령을 찾을 수 없음</h2>
-        <h3 className="mt-[-8px]">people과의 연결을 거부했습니다.</h3>
+        <div className="text-[1.8rem] text-red-700">
+          <span className="text-black">[404 Error!]</span>
+          <br />
+          <h2 className="mt-[-10px]">대통령을 찾을 수 없음</h2>
+        </div>
+        <h3 className="mt-[-3px]">people과의 연결을 거부했습니다.</h3>
       </article>
 
       <article className="flex flex-col gap-2">
         <h4>다음 방법을 시도해보세요</h4>
         <ul>
           <li>60일 내 대선</li>
-          <li>국민의짐? 내란의힘? 뭐 비슷한거 해체;</li>
+          <li>국민의짐? 내란의힘? 뭐 비슷한거 해산;</li>
         </ul>
         <h5 className="text-[#bbb]">WIN_250404_1122_FROM_241203</h5>
       </article>
